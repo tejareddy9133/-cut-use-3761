4e9c4ba06db3dd22235dea70dac1ba8a1d5e54e2
tejareddy9133 <reddyvaritejeshkumarreddy@gmail.com>
2023-06-14 22:46:42 +0530

    subscription

--- frontend/src/routes/Mainroutes.jsx
+++ frontend/src/routes/Mainroutes.jsx
@@ -4,12 +4,13 @@ import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
 import Login from "../pages/LoginPage";
 import Signup from "../pages/SignUpPage";
-
+import Subscription from "../pages/Subscription";
 const Mainroutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/subscription" element={<Subscription />}></Route>
       <Route path="/signup" element={<Signup />} />
       <Route path="*" element={<h1>404 Page Not Found</h1>} />
     </Routes>
