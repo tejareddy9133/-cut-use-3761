fbdd3225053f9e3789588ae4b822db29c55b33d6
NainaPremani <nainapremani9@gmail.com>
2023-06-14 22:42:07 +0530

    pushing main

--- frontend/src/routes/Mainroutes.jsx
+++ frontend/src/routes/Mainroutes.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { HomePage } from "../pages/HomePage";
-import { LoginPage } from "../pages/LoginPage";
-import { SignUpPage } from "../pages/SignUpPage";
+import Login from "../pages/LoginPage";
+import Signup from "../pages/SignUpPage";
 
 const Mainroutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage/>} />
-      <Route path="/login" element={<LoginPage/>} />
-      <Route path="/signup" element={<SignUpPage/>} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="*" element={<h1>404 Page Not Found</h1>} />
     </Routes>
-
   );
 };
 
