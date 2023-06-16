3321ac6f03430c5b3795a77c3cb995f69c431ba5
ravi shukla <ravi21.434@gmail.com>
2023-06-16 22:51:18 +0530

    day4

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,104 @@
 import React from "react";
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import SubscriptionsOutlinedIcon from '@mui/icons-material/SubscriptionsOutlined';
+//npm install @mui/icons-material @mui/material @emotion/styled @emotion/react
+
+import "../css/Footer.css"
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return <div style={{ background: "rgb(12,15,18)", color: "rgb(255,255,255)", paddingBottom: "20px" ,paddingTop:"20px"}}>
+
+
+    <div style={{ boxShadow: "rgba(239,239,239,0.65) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset", color: "white", padding: "20px 40px", margin: "2px 40px", borderRadius: "30px", textAlign: "start", position: "relative" }}>
+      <div className="hide123" style={{ position: "absolute", right: "40px", fontSize: "100px", border: "0px solid red", top: "-40px", color: "green" }}><i>&#x266C;</i></div>
+
+      {/*    <div style={{position:"absolute",right:"40px",fontSize:"100px",border:"0px solid red",top:"-40px",color:"green"}}><i>&#x266C;</i></div>   */}
+
+      <div style={{ textAlign: "center" }}>
+        <span style={{ fontSize: "30px", color: "white", textAlign: "left", justifyContent: "left", }}>About Vibe Tunes <i>&#x266C;</i></span>
+
+
+      </div>
+
+
+
+      <br />
+      <span style={{ fontFamily: "sans-serif", color: "white" }}> Vibe Tunes is a complete package that allows you free online music streaming, set caller tunes, listen to podcasts, download MP3 music offline, and much more! Since music is essentially the only thing that can truly understand a person, we consistently offer our audience the ideal blend of MP3 Songs by their favourite artists and of versatile genres.  </span>
+
+      <br />
+      <span style={{ fontFamily: "sans-serif", color: "white" }}> Vibe Tunes is a complete package that allows you free online music streaming, set caller tunes, listen to podcasts, download MP3 music offline, and much more! Since music is essentially the only thing that can truly understand a person, we consistently offer our audience the ideal blend of MP3 Songs by their favourite artists and of versatile genres.  </span>
+
+
+
+    </div>
+
+
+    <div style={{ margin: "20px 40px" }}>
+
+
+      <div className="footerpart" style={{ display: "flex", flexDirection: "row", columnGap: "0px", fontFamily: "sans-serif", textAlign: "start" }}>
+        <div style={{ boxShadow: "rgba(239,239,239,0.65) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset", borderRadius: "30px", gap: "20px", margin: "20px 20px", padding: "20px 20px" }}>
+          <h1 style={{ fontFamily: "sans-serif", fontWeight: "bolder" }}>LATEST SONGS</h1>
+          <p>Barsaat Aa Gayi  |  Tum Se Milke (From "Tiku Weds Sheru")  |  Aaj Ke Baad (From "Satyaprem Ki Katha")  |  Hai Tu (From "I Love You")  |  Deewani  |  Woh Kahani  |  Yeh Nazar (From "I Love You")  |  Khwaab Tha Tu Mera  |  Naseeb Se (From "Satyaprem Ki Katha")  |  Baarish Aayi Hai  |  Yaad Teri Aaye Baar Baar  |  Door  |  Issa Vibe  |  Madari (feat. Karan Kanchan)  |  King Of Indian Hip Hop</p>
+
+
+        </div>
+        <div style={{ boxShadow: "rgba(239,239,239,0.65) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset", borderRadius: "30px", gap: "20px", margin: "20px 20px", padding: "20px 20px" }}>
+          <h1 style={{ fontFamily: "sans-serif", fontWeight: "bolder" }}>LATEST SONGS</h1>
+          <a style={{ fontFamily: "sans-serif" }}>Barsaat Aa Gayi  |  Tum Se Milke (From "Tiku Weds Sheru")  |  Aaj Ke Baad (From "Satyaprem Ki Katha")  |  Hai Tu (From "I Love You")  |  Deewani  |  Woh Kahani  |  Yeh Nazar (From "I Love You")  |  Khwaab Tha Tu Mera  |  Naseeb Se (From "Satyaprem Ki Katha")  |  Baarish Aayi Hai  |  Yaad Teri Aaye Baar Baar  |  Door  |  Issa Vibe  |  Madari (feat. Karan Kanchan)  |  King Of Indian Hip Hop</a>
+
+
+        </div>
+        <div style={{ boxShadow: "rgba(239,239,239,0.65) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset", borderRadius: "30px", gap: "20px", margin: "20px 20px", padding: "20px 20px" }}>
+          <h1 style={{ fontFamily: "sans-serif", fontWeight: "bolder" }}>LATEST SONGS</h1>
+          <a style={{ fontFamily: "sans-serif" }}>Barsaat Aa Gayi  |  Tum Se Milke (From "Tiku Weds Sheru")  |  Aaj Ke Baad (From "Satyaprem Ki Katha")  |  Hai Tu (From "I Love You")  |  Deewani  |  Woh Kahani  |  Yeh Nazar (From "I Love You")  |  Khwaab Tha Tu Mera  |  Naseeb Se (From "Satyaprem Ki Katha")  |  Baarish Aayi Hai  |  Yaad Teri Aaye Baar Baar  |  Door  |  Issa Vibe  |  Madari (feat. Karan Kanchan)  |  King Of Indian Hip Hop</a>
+
+
+        </div>
+        <div style={{ boxShadow: "rgba(239,239,239,0.65) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset", borderRadius: "30px", gap: "20px", margin: "20px 20px", padding: "20px 20px" }}>
+          <h1 style={{ fontFamily: "sans-serif", fontWeight: "bolder" }}>LATEST SONGS</h1>
+          <a style={{ fontFamily: "sans-serif" }}>Barsaat Aa Gayi  |  Tum Se Milke (From "Tiku Weds Sheru")  |  Aaj Ke Baad (From "Satyaprem Ki Katha")  |  Hai Tu (From "I Love You")  |  Deewani  |  Woh Kahani  |  Yeh Nazar (From "I Love You")  |  Khwaab Tha Tu Mera  |  Naseeb Se (From "Satyaprem Ki Katha")  |  Baarish Aayi Hai  |  Yaad Teri Aaye Baar Baar  |  Door  |  Issa Vibe  |  Madari (feat. Karan Kanchan)  |  King Of Indian Hip Hop</a>
+
+
+        </div>
+
+      </div>
+
+
+
+    </div>
+
+
+    <div className="last" style={{ display: "flex", flexDirection: "row", columnGap: "10px", margin: "2px 50px", textAlign: "center", justifyContent: "space-around" }}>
+      <diV style={{ display: "flex", flexDirection: "row", columnGap: "50px" }}>
+        <h1>ABOUT US</h1>
+
+        <h1>PRIVACY POLICY</h1>
+        <h1>ABOUT US</h1>
+        <h1>PRIVACY POLICY</h1>
+        <h1>ABOUT US</h1>
+        <h1>PRIVACY POLICY</h1>
+
+      </diV>
+
+      <div style={{ display: "flex", flexDirection: "row", columnGap: "10px" }}>
+        <FacebookOutlinedIcon />
+        <EmailOutlinedIcon />
+        <SubscriptionsOutlinedIcon />
+
+
+
+      </div>
+
+
+
+
+    </div>
+
+
+
+
+
+  </div>;
 };
