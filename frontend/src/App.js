ee34bb75c21bda437689d1a3a854403629b29769
tejareddy9133 <reddyvaritejeshkumarreddy@gmail.com>
2023-06-18 00:23:04 +0530

    playlist page ready  for use

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,19 @@ import "slick-carousel/slick/slick-theme.css";
 import Navbar from "./components/Navbar";
 import Mainroutes from "./routes/Mainroutes";
 
-
 import { Footer } from "./components/Footer";
-
+import { Spotify } from "./pages/Spotify";
+import Songitem from "./components/Songitem";
+import Playlist from "./pages/Playlist";
 
 function App() {
   return (
     <div className="App">
+      <Playlist />
+      <Songitem />
+      {/* <Mainroutes/>
 
-    
-      <Mainroutes/>
-
-      <Footer/>
-
-      
+      <Footer/> */}
     </div>
   );
 }
--- frontend/src/components/Songitem.jsx
+++ frontend/src/components/Songitem.jsx
@@ -1,31 +1,74 @@
 import React from "react";
 import { Box, Heading } from "@chakra-ui/react";
+import { DeleteIcon } from "@chakra-ui/icons";
 
 const Songitem = () => {
   return (
     <div>
-      <Box
-        bg="black"
-        h={"20"}
-        w="75%"
-        p={0}
-        color="white"
-        display={"flex"}
-        justifyContent={"space-around"}
+      <div
+        style={{
+          display: "flex",
+          color: "white",
+          padding: "10px",
+          backgroundColor: "black",
+        }}
       >
-        <Box bg="black" h={"20"} w="100%" p={0} color="white">
+        <div
+          style={{
+            height: "80px",
+            width: "100%",
+            backgroundColor: "black",
+            display: "flex",
+            gap: "2%",
+            lineHeight: "24px",
+          }}
+        >
           <img
             src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_saregama/20230614165945000/8907212007012/1686816917782/resources/8907212007012.jpg"
             alt=""
             style={{ height: "100%", width: "10%" }}
           />
-        </Box>
-        <Box>
-          <Heading as="h3" size="lg">
-            (lg) In love with React & Next
-          </Heading>
-        </Box>
-      </Box>
+          <p
+            style={{
+              fontSize: "18px",
+              fontFamily: "sans-serif,Roboto",
+              fontStretch: "extra-expanded",
+            }}
+          >
+            Zara In love with React & Next <br />{" "}
+            <span>
+              <p
+                style={{
+                  fontSize: "18px",
+                  fontFamily: "sans-serif,Roboto",
+                  fontStretch: "extra-expanded",
+                  color: "GrayText",
+                  textDecoration: "underline",
+                  marginTop: "10px",
+                }}
+              >
+                ArjithSing
+              </p>
+            </span>
+          </p>
+        </div>
+        <div style={{ color: "white", display: "flex", gap: "40px" }}>
+          <button style={{}}>
+            <DeleteIcon />
+          </button>
+          <button
+            style={{
+              backgroundColor: "red",
+              width: "50px",
+              height: "30px",
+              marginTop: "27px",
+              borderRadius: "20%",
+            }}
+          >
+            <h4>play</h4>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
